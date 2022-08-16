5649ac5b9deb445342ddf58dbcb403469ea0376f
MikhailYuzhakov <miha.yuzhakov@gmail.com>
2022-08-17 00:27:21 +0300

    Крестики-нолики

--- Task3/Task3.py
+++ Task3/Task3.py
@@ -2,12 +2,13 @@
 
 from random import randint
 
-board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]] # виртуальное игровое поле
 
-if (randint(0,1) == 0): 
+if (randint(0,1) == 0): # выбираем очередность хода
     flag = False
 else: flag = True
 
+# функция для печать игрового поля
 def printBoard(board):
     print(chr(27) + "[2J") # очистка экрана 
     for i in range(3):
@@ -20,62 +21,106 @@ def printBoard(board):
                 print("0", " ", end = '')
         print()
 
+# функция проверяет условие победы
 def checkWin():
     global board
-    sum = 0
-
-    for i in range(3): # проверяем каждый столбец  
-        for j in range(3): 
-            sum += board[i][j] 
-        if (abs(sum) == 3): 
-            print(abs(sum))
-            return 1
-    
-    for i in range(3): # проверяем каждую строку
-        for j in range(3): 
-            sum += board[i][j]
-        if (abs(sum) == 3): 
-            print(abs(sum))
-            return 1
-    
-    # проверяем диагонали
-    if (abs(board[0][0] + board[1][1] + board[2][2]) == 3): 
-        print(abs(sum))
-        return 1
-    if (abs(board[0][2] + board[1][1] + board[2][0]) == 3): 
-        print(abs(sum))
-        return 1
-
-    return 0
-
-def game():
     global flag
-    printBoard(board)
-    print(checkWin())
-    if (flag):
-        i = int(input())
-        j = int(input())
-        if (board[i][j] == 0): board[i][j] = 1
-        flag = not flag
-        if (checkWin()): 
-            printBoard(board)
-            print("Победили Х")
-            return
-        else:
-            game()
-            
-    else:
-        i = int(input())
-        j = int(input())
-        if (board[i][j] == 0): board[i][j] = -1
+    
+    sum = 0
+    for i in range(3): # проверяем каждую строку  
+        for j in range(3): 
+            sum += board[i][j]      
+        # print("Сумма по строкам:", sum)
         
-        flag = not flag
-        if (checkWin()): 
+        if (sum == -3):
             printBoard(board)
             print("Победили 0")
             return
-        else:
-            game()
+        if (sum == 3):
+            printBoard(board)
+            print("Победили X")
+            return
+        sum = 0 # обнуляем переменную для следующем итерации
+    
+    sum = 0
+    for i in range(3): # проверяем каждую столбец
+        for j in range(3): 
+            sum += board[j][i]
+        # print("Сумма по столбцам", sum)
+
+        if (sum == -3):
+            printBoard(board)
+            print("Победили 0")
+            return
+        if (sum == 3):
+            printBoard(board)
+            print("Победили X")
+            return
+        sum = 0 # обнуляем переменную для следующем итерации
+    
+    # проверяем диагонали
+    sum = 0
+    if (board[0][0] + board[1][1] + board[2][2] == -3):
+        # print("Сумма по гл. диагоналям", board[0][0] + board[1][1] + board[2][2])
+        printBoard(board)
+        print("Победили 0")
+        return
+    if (board[0][0] + board[1][1] + board[2][2] == 3):
+        # print("Сумма по гл. диагоналям", board[0][0] + board[1][1] + board[2][2])
+        printBoard(board)
+        print("Победили X")
+        return
+    
+    sum = 0
+    if (board[0][2] + board[1][1] + board[2][0] == -3):
+        # print("Сумма по втор. диагоналям", board[0][2] + board[1][1] + board[2][0]) 
+        printBoard(board)
+        print("Победили 0")
+        return
+    if (board[0][2] + board[1][1] + board[2][0] == -3):
+        # print("Сумма по втор. диагоналям", board[0][2] + board[1][1] + board[2][0]) 
+        printBoard(board)
+        print("Победили X")
+        return
+
+    flag = not flag # передаем ход ноликам
+    game() # если никто не победил запускаем игру
+
+def game():
+    global flag
+    printBoard(board) # печатаем игровое поле
+
+    if (flag): # первые ходят крестики
+        print("Ходят крестики. Ввелите позицию строки и столбца через Enter:")
+        i = int(input()) # считываем позицию строки
+        while (i > 2 or i < 0): # проверка на корректность введенных данных
+            print("Такой позиции на игровом поле нет. Введите позицию строки и столбца через Enter:")
+            i = int(input()) # считываем позицию строки
+
+        j = int(input()) # считываем позицию столбца
+        while (j > 2 or j < 0): # проверка на корректность введенных данных
+            print("Такой позиции на игровом поле нет. Введите позицию строки и столбца через Enter:")
+            j = int(input()) # считываем позицию строки
+        
+        if (board[i][j] == 0): board[i][j] = 1 # если поле не заполнено, то вписываем крестик (лог. 1)
+        
+        checkWin() # проверяем условие победы (передать флаг в качестве ключа)
+            
+    else: # первые ходят нолики
+        print("Ходят нолики. Введите позицию строки и столбца через Enter:")
+        i = int(input()) # считываем позицию строки
+        while (i > 2 or i < 0): # проверка на корректность введенных данных
+            print("Такой позиции на игровом поле нет. Введите позицию строки и столбца через Enter:")
+            i = int(input()) # считываем позицию строки
+
+        j = int(input()) # считываем позицию столбца
+        while (j > 2 or j < 0): # проверка на корректность введенных данных
+            print("Такой позиции на игровом поле нет. Введите позицию строки и столбца через Enter:")
+            j = int(input()) # считываем позицию столбца
+
+        if (board[i][j] == 0): board[i][j] = -1 # если поле не заполнено, то вписываем крестик (лог. -1)
+
+        checkWin() # проверяем условие победы (передать флаг в качестве ключа)
 
 game()
 
